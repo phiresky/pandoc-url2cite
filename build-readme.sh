51213dc58393a1ae95b59ac4366505920d16a9cf
phiresky <phireskyde+git@gmail.com>
2019-12-12 21:05:46 +0100

    add some more documentation

--- build-readme.sh
+++ build-readme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm run build
+npm run build # compile typescript
 pandoc README.md -o README.pdf --csl example/ieee-with-url.csl \
     --filter=./dist/index.js --filter=pandoc-citeproc --highlight-style zenburn
