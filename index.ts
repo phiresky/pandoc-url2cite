5b471df0dc2d93bd7c35e86c052b9defbadb77b8
phiresky <phireskyde+git@gmail.com>
2019-12-12 21:55:17 +0100

    shebang

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /// <reference path="untyped.d.ts" />
 
 import cjs from "citation-js";
--- pandoc-url2cite.ts
+++ pandoc-url2cite.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import * as fs from "fs";
 import { Url2Cite } from "./index";
 
