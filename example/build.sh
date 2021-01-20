f590289137351a8dc1c789e71de3fa9d59a805f7
phiresky <phireskyde+git@gmail.com>
2021-01-20 11:56:45 +0100

    add other output formats to example

--- example/build.sh
+++ example/build.sh
@@ -1,6 +1,8 @@
 npm run build
 
-pandoc  --filter=../dist/pandoc-url2cite.js --citeproc minimal.md \
-    --csl ieee-with-url.csl -o minimal.pdf
+for output_format in pdf odt docx; do
+	pandoc  --filter=../dist/pandoc-url2cite.js --citeproc minimal.md \
+	    --csl ieee-with-url.csl -o minimal.$output_format
+done
 pandoc  --filter=../dist/pandoc-url2cite.js --citeproc doi-isbn.md \
     --csl apa.csl -o doi-isbn.pdf
